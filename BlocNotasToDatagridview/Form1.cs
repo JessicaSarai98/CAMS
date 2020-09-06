db8cfde6940574c727be8f0b6df832f76f633f86
Jessica González Bautista <jessicabautista1698@gmail.com>
2020-09-06 13:36:58 -0500

    Pase de lista

---
+++ BlocNotasToDatagridview/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlocNotasToDatagridview
+{
+    public partial class Form1 : Form
+    {
+        //Instancia de la clase Leer
+        Leer l = new Leer();
+        //Alamcena la ruta del archivo .txt
+        public string ARCHIVO ="";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        //Abre el openFileDialog y captura la ruta del bloc de notas'
+        public void cargarArchivo()
+        {
+            try
+            {
+                this.openFileDialog1.ShowDialog();
+
+                if (!string.IsNullOrEmpty(this.openFileDialog1.FileName))
+                {
+                    ARCHIVO = this.openFileDialog1.FileName;
+                    l.lecturaArchivo(dataGridView1, ',', ARCHIVO);
+
+                    dataGridView1.Columns[0].HeaderText = "Nombre";
+                    dataGridView1.Columns[1].HeaderText = "Escuela";
+                    dataGridView1.Columns[2].HeaderText = "Estado";
+
+                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma",9.75F, FontStyle.Bold); 
+
+                    dataGridView1.Columns[3].Visible = false;
+                    dataGridView1.Columns[4].Visible = false;
+
+                    DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+                    chk.HeaderText = "Elegir";
+                    dataGridView1.Columns.Add(chk);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: "+ex.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cargarArchivo();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
