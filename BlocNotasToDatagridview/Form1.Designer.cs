38d155a1175d1128bf3a99f4a0806256074403bb
Jessica González Bautista <jessicabautista1698@gmail.com>
2020-09-09 20:31:30 -0500

    Pase de lista terminado

--- BlocNotasToDatagridview/Form1.Designer.cs
+++ BlocNotasToDatagridview/Form1.Designer.cs
@@ -64,6 +64,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(459, 223);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValeChanged);
             // 
             // openFileDialog1
             // 
--- BlocNotasToDatagridview/Form1.cs
+++ BlocNotasToDatagridview/Form1.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Collections.Generic;
-
+using System.Linq; 
 
 namespace BlocNotasToDatagridview
 {
@@ -11,7 +11,7 @@ namespace BlocNotasToDatagridview
         //Instancia de la clase Leer
         Leer l = new Leer();
         //Alamcena la ruta del archivo .txt
-        public string ARCHIVO ="";
+        public string ARCHIVO = "";
 
         public Form1()
         {
@@ -20,7 +20,7 @@ namespace BlocNotasToDatagridview
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         //Abre el openFileDialog y captura la ruta del bloc de notas'
@@ -39,7 +39,7 @@ namespace BlocNotasToDatagridview
                     dataGridView1.Columns[1].HeaderText = "Escuela";
                     dataGridView1.Columns[2].HeaderText = "Estado";
 
-                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma",9.75F, FontStyle.Bold); 
+                    dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
 
                     dataGridView1.Columns[3].Visible = false;
                     dataGridView1.Columns[4].Visible = false;
@@ -47,35 +47,101 @@ namespace BlocNotasToDatagridview
                     DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
                     chk.HeaderText = "Elegir";
                     dataGridView1.Columns.Add(chk);
+
+
+
                 }
 
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: "+ex.ToString());
+                MessageBox.Show("Error: " + ex.ToString());
             }
         }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             cargarArchivo();
         }
         int contador = 0;
-        string[] nombres = new string[11];
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        string[] nombres = new string[20];
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) 
         {
-            if (contador < 10)
+            
+                int posicion = -1;
+                string nombre = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                bool encontradoNombre = false;
+                if (contador > 0)
+                {
+                    for (int i = 0; i < contador; i++)
+                    {
+                        if (nombres[i].Equals(nombre))
+                        {
+                            nombres[i] = null;
+                            contador--;
+                            MessageBox.Show("" + contador);
+                            encontradoNombre = true;
+                            posicion = i;
+                        }
+                    }
+                }
+                if (encontradoNombre == false)
+                {
+                    nombres[contador] = nombre;
+                    contador++;
+                    MessageBox.Show("" + contador);
+                }
+                if (encontradoNombre)
+                {
+                    for (int j = posicion; j < contador; j++)
+                    {
+                        if (posicion != 19 && nombres[j + 1] != null)
+                        {
+                            String aux = nombres[j + 1];
+                            nombres[j] = aux;
+                        }
+                    }
+                }
+
+            if (contador == 10)
             {
-                contador += 1;
-                string a = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
-                nombres[contador] = a;
-            }            
-            if(contador == 10)
-            {
-                MessageBox.Show("Usted ya ha seleccionado 10 participantes.");
                 btnTerminar.Visible = true;
+
             }
+            else
+            {
+                btnTerminar.Visible = false;
+            }
+
+            
+
+            //    if (contador < 10)
+            //    {
+            //        contador += 1;
+            //        string a = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            //        nombres[contador] = a;
+            //        MessageBox.Show(""+contador);
+
+            //        if (contador == 10)
+            //        {
+            //            MessageBox.Show("Usted ya ha seleccionado 10 participantes.");
+            //            btnTerminar.Visible = true;
+            //        }
+            //if (contador > 10)
+            //{
+            //    contador -= 1;
+            //    string b = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            //    nombres[contador] = b;
+            //}
+            // }
+
+
         }
+
 
         Random rnd = new Random();
         List<int> listaNoRepetidos = new List<int>();
@@ -84,19 +150,25 @@ namespace BlocNotasToDatagridview
         ParejasAleatorias Parejas = new ParejasAleatorias();
         int valor1 = 0;
         int valor2;
+
+        int cont = 0;
+
+
         private void btnTerminar_Click(object sender, EventArgs e)
         {
+
+
             listaNoRepetidos.Clear();
             IntialCount = 1;
             var result = MessageBox.Show("A continuación se generá la lista de parejas con los " +
                 "participantes seleccionados. ¿Desea continuar?", "Mensaje", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
             if (result == DialogResult.OK)
-            {                
+            {
                 while (IntialCount <= 10)
                 {
                     //GENERAMOS EL NÚMERO ALEATORIO
                     rnd = new Random();
-                    int aux = Convert.ToInt32(rnd.Next(1, contador + 1));
+                    int aux = Convert.ToInt32(rnd.Next(0, contador));
                     //si no esta en la lista, lo anexamos y sí evitamos que salgan nombres
                     // o numero repetidos en el algoritmo
                     if (!listaNoRepetidos.Contains(aux))
@@ -104,11 +176,11 @@ namespace BlocNotasToDatagridview
                         listaNoRepetidos.Add(aux);
                         IntialCount++;
                         contValor++;
-                        if(contValor == 1)
+                        if (contValor == 1)
                         {
                             valor1 = aux;
                         }
-                        if(contValor == 2)
+                        if (contValor == 2)
                         {
                             valor2 = aux;
                             contValor = 0;
@@ -117,8 +189,48 @@ namespace BlocNotasToDatagridview
                     }
 
                 }
-
+               
                 Parejas.Show();
+                
+            }
+
+
+
+        }
+
+        private void dataGridView1_CellValeChanged(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private bool encontrado(String[] nombres, String nombre, int contador)
+        {
+            if (contador!=0)
+            {
+                for (int i = 0; i < nombres.Length; i++)
+                {
+                    if (nombres[i].Equals(nombre))
+                    {
+                        nombres[i] = null;
+                        MessageBox.Show("Eliminando a " + nombre);
+                        organizar(nombres, i);
+                        return true;
+                    }
+                }
+            }
+            nombres[contador] = nombre;
+            MessageBox.Show("Agregando a "+ nombre);
+            return false;
+        }
+
+        private void organizar(String[] nombres, int i)
+        {
+            for (int j = i; j<nombres.Length; j++)
+            {
+                if (i!=9 && nombres[i+1]!=null)
+                {
+                    String aux = nombres[i + 1];
+                    nombres[i] = aux;
+                }
             }
         }
     }
